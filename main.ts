b52e9360d97ad6e4e570145c243e7e6866a31699
t-mario-y <t.mario.yoshi@gmail.com>
2021-08-23 10:14:04 +0900

    Revert "Merge pull request #14 from t-mario-y/release/delete-file-with-hotfix"
    This reverts commit 0d949337b65435680f0f4e777876dae3f1b8451a, reversing
    changes made to 09d7e25e54c1dd64bb8c83a8b7d1f467c6116432.

--- main.ts
+++ main.ts
@@ -9,5 +9,3 @@ console.log("this is feature/5");
 console.log("this is feature/6");
 
 console.log("this is feature/7");
-
-console.log("this is feature/8");
--- willBeDeleted.ts
+++
@@ -1,3 +0,0 @@
-console.log("This file will be deleted with hotfix.");
-
-console.log("some parameter is e551e92b84c5b17ab496fcede6f2710f.");
